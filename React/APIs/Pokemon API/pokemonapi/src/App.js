17769b4543d433f76f8cc957227ecdeb8d59d53a
Shtaiwee <shtaiweeomair@gmail.com>
2022-04-15 22:10:50 +0300

    assignment

---
+++ React/APIs/Pokemon API/pokemonapi/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Pokemons from './components/Pokemons'
+
+function App() {
+  return (
+    <div className='App'>
+      <Pokemons/>
+    </div>
+  )
+}
+
+export default App
