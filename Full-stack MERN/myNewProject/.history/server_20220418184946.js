936f9f00b79f1e497c485c38f0639d0884216db3
Mohammad Omair <shtaiweeomair@gmail.com>
2022-04-20 03:14:19 +0300

    Delete Full-stack MERN/myNewProject directory

--- Full-stack MERN/myNewProject/.history/server_20220418184946.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-require('./server/routes/person.routes')(app); // This is new
-app.listen(8000, () => {
-    console.log("Listening at Port 8000")
-})
