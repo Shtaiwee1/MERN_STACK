395e368ceab41ef00690f6eeb28bddb4ada7e39d
Shtaiwee <shtaiweeomair@gmail.com>
2022-04-20 03:11:35 +0300

    Full stack MERN

---
+++ Full-stack MERN/Full-Stack MERN/Product Manager/.history/productmanager/src/views/Detail_20220419022600.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { useParams } from "react-router-dom";
+import { Link } from 'react-router-dom';
+    
+const Detail = (props) => {
+    const [product, setProduct] = useState({})
+    const { id } = useParams();
+    
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/products/' +id)
+            .then(res => setProduct(res.data))
+            .catch(err => console.error(err));
+    }, []);
+
+    const deleteProduct = (productId) => {
+        axios.delete('http://localhost:8000/api/products/' + productId)
+            .then(res => {
+                removeFromDom(productId)
+            })
+            .catch(err => console.error(err));
+    }
+    
+    return (
+        <div>
+            <p><strong>Title:</strong> {product.title}</p>
+            <p><strong>Price:</strong> {product.price}</p>
+            <p><strong>Description:</strong> {product.description}</p>
+            <Link to={"/products/" + product._id + "/edit"}>Edit</Link>
+        </div>
+    )
+}
+    
+export default Detail;
